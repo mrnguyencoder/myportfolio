03bda176c86115a217481ff6ddc954b4877db600
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-08 18:54:00 +0100

    set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home  from './pages/Home';
+import Projects from './pages/Projects';
+import Contact  from './pages/Contact';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
- 
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path= "/" element={<Home />} />
+          <Route path= "/projects" element={<Projects />} />
+          <Route path= "/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
