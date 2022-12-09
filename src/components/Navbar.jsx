41a7fdb2d0c37e7684680a782978ca373b261c8e
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-09 11:22:41 +0100

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../assets/textnguyencoder.png';
 
 
 
 function Navbar() {
   return (
     <div className=''>
-        <div className="logo">
-            <img src="" alt="" />
-        </div>
+        <Link className="logo" to="/">
+            <img src={Logo} alt="logo nguyen coder" />
+        </Link>
         <div className="links">
-            <Link className="link" to="/">Home</Link>
+            <Link className="link" >Home</Link>
             <Link className="link" to="/projects">Projects</Link>
             <Link className="link" to="/contact">Contact</Link>
         </div>
