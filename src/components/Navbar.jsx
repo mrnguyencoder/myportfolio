d8c6a3e825306a487a76c3827f0cd1c55cfd34de
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-09 15:49:02 +0100

    framer-motion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../assets/textnguyencoder.png';
 
-
+const style = {
+    navbar:`
+    bg-green-400 shadow`,
+    logo: `
+    `
+}
 
 function Navbar() {
   return (
-    <div className=''>
-        <Link className="logo" to="/">
+    <nav className={style.navbar}>
+        <Link className={style.logo} to="/">
             <img src={Logo} alt="logo nguyen coder" />
         </Link>
-        <div className="links">
+        <div className={style.links}>
             <Link className="link" >Home</Link>
             <Link className="link" to="/projects">Projects</Link>
             <Link className="link" to="/contact">Contact</Link>
         </div>
-    </div>
+    </nav>
   )
 }
 
