93ff425695574c61cd993d1393dc38cb2768b466
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-18 01:35:53 +0100

    contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,69 @@
 import React from 'react';
+import { FaAddressCard, FaPhoneVolume } from "react-icons/fa";
+import { SiGmail } from 'react-icons/si';
 import { useForm } from "react-hook-form";
-import { motion } from "framer-motion";
-import contact from '../assets/contact.png'
+import { motion } from 'framer-motion';
 
 function Contact() {
+  const { handleSubmit, register} = useForm();
+  const onSubmit = values => console.log(values);
 
   return (
-    <section className=''>
-     
-    </section>
+    <motion.section className=''
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ one: true, amount: 0.5 }}
+      transition ={{ duration: 0.5 }}
+      variants={{
+        hidden: {opacity: 0 , x:-50},
+        visible: { opacity: 1, x: 0},
+      }}
+    >
+     <div className="flex flex-col space-y-10 text-center">
+      <p className="text-4xl text-amber-400 pt-8 underline">
+        Contact me to get started</p>
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <FaPhoneVolume className='h-7 w-7 animate-pulse' />
+            <p className="text-2xl">+33-782885224</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <SiGmail className='h-7 w-7 animate-pulse'/>
+            <p className="text-2xl">mr.nguyencoder@gmail.com</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <FaAddressCard className='h-7 w-7 animate-pulse'/>
+            <p className="text-2xl">Paris, France</p>
+          </div>
+        </div>
+        <form 
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col space-y-2 w-fit mx-auto"
+        >  
+          <div className='flex space-x-2'>
+            <input placeholder='Name' className='outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-amber-300 text-gray-500 transition-all focus:border-amber-500 focus:text-gray-800' type="text"
+                {...register("name", {
+                  validate: value => value !== "admin" || "Nice try!"
+                })}
+            />
+            <input placeholder='Email' className='outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-amber-300 text-gray-500 transition-all focus:border-amber-500 focus:text-gray-800' type="email" 
+              {...register("email", {
+                required: "Required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "invalid email address"
+                }
+              })}      
+            />
+          </div>
+          <input placeholder='Subject' className='outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-amber-300 text-gray-500 transition-all focus:border-amber-500 focus:text-gray-800' type="text"
+                {...register("subject")}
+          />
+          <textarea {...register("message")} placeholder='Message' className='outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-amber-300 text-gray-500 transition-all focus:border-amber-500 focus:text-gray-800'/>
+          <button type='submit' className='bg-green-400 py-5 px-10 rounded-md font-bold text-lg'>Submit</button>
+        </form>
+     </div>
+    </motion.section>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,25 +5,25 @@ import { FcAbout, FcAssistant, FcHome, FcNews } from "react-icons/fc";
 
 function Home() {
   return (
-    <div className='flex w-full'>
+    <div className='flex w-full h-full'>
       <div className="hidden w-[8%] m-3 md:flex flex-col items-center justify-around border-b border-blue-500">
         <p className="flex flex-col justify-center items-center"> <FcHome />Home</p>
         <p className="flex flex-col justify-center items-center"> <FcAbout />About</p>
         <p className="flex flex-col justify-center items-center"> <FcNews />Blog</p>
         <p className="flex flex-col justify-center items-center"> <FcAssistant />Contact</p>
       </div>
-      <div className="w-full md:w-[92%] m-3 rounded-xl bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
-        <div className="p-6">
+      <div className="w-full md:w-[92%] ">
+        <div className="m-3 rounded-xl bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 p-6">
           <h1 className="text-2xl sm:text-6xl py-4
                          text-transparent font-bold bg-clip-text
                          bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-                <span class="inline-flex">👋</span>Nguyen Coder # JavaScript Dev     
+                <span class="inline-flex">👋</span>Nguyen Coder     
           </h1>
-            <p className='text-orange-300'>Base in Paris, make code with love
+            <p className='text-orange-300'>JavaScript Dev
               <span class="animate-bounce inline-flex">👋</span>
             </p>
             <p className="text-2xl md:text-3xl py-5 text-teal-400 leading-7">
-              I have been coding for 1 years. Love to make a wed-app with personal request</p>
+            Base in Paris, make code with love</p>
             <div className="text-xl flex justify-end gap-8 py-3 pr-3 text-emerald-300">
                 <FiLinkedin />
                 <FiGithub />
@@ -31,7 +31,25 @@ function Home() {
                 <FiBox />
             </div>
         </div>
+
+        <div class="m-3 rounded-xl p-6 bg-gray-50">
+          <div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              <span class="block">Ready to dive in?</span>
+              <span class="block text-indigo-600">Start your free trial today.</span>
+            </h2>
+            <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+              <div class="inline-flex rounded-md shadow">
+                <p class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700">Get started</p>
+              </div>
+              <div class="ml-3 inline-flex rounded-md shadow">
+                <p class="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50">Learn more</p>
+              </div>
+          </div>
+        </div>
       </div>
+
+    </div>
       
       {/* <section className='w-full h-screen text-center flex flex-col items-center justify-center px-4 ms:px-8 xl:px-12'>
           <h2 className="text-4xl sm:text-6xl py-4
