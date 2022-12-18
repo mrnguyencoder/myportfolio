7fb4f26779475a3f7c95fc0197317218a2165634
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-18 03:17:03 +0100

    home section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,6 +13,69 @@ function Home() {
         <p className="flex flex-col justify-center items-center"> <FcAssistant />Contact</p>
       </div>
       <div className="w-full md:w-[92%] ">
+        <div className="container mx-auto
+                        bg-gradient-to-r from-[#f0db4f] to-[#61dbfb]
+                        m-3 p-6 shadow-md
+        ">
+          <p className="text-5xl text-[#3C873A] font-bold text-center mb-6">Welcome to My Site</p>
+          <p className="text-3xl text-center text-blue-800 mb-4">
+            Front-end Dev 
+          </p>
+          <p className="text-xl text-center text-blue-600 mb-4">
+            Base in Paris, make code with love
+          </p>
+
+      <div className="container mx-auto py-6">
+        <h2 className="text-3xl font-bold mb-6 text-center">Connect with Me</h2>
+        <div className="flex justify-center mb-6">
+          <a
+            href="#"
+            className="text-gray-700 hover:text-blue-500 mx-4"
+          >
+            <i className="fab fa-twitter fa-2x"></i>
+          </a>
+          <a
+            href="#"
+            className="text-gray-700 hover:text-blue-500 mx-4"
+          >
+            <i className="fab fa-github fa-2x"></i>
+          </a>
+          <a
+            href="#"
+            className="text-gray-700 hover:text-blue-500 mx-4"
+          >
+            <i className="fab fa-linkedin fa-2x"></i>
+          </a>
+          <a
+            href="#"
+            className="text-gray-700 hover:text-blue-500 mx-4"
+          >
+            <i className="fab fa-instagram fa-2x"></i>
+          </a>
+        </div>
+        <p className="text-gray-700 text-center mb-6">
+          Or send me an email at <a className="text-blue-500" href="mailto:your@email.com">your@email.com</a>
+        </p>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+          <div className="text-center">
+            <button className="bg-[#61dbfb] hover:bg-[#3C873A] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              Learn More
+            </button>
+          </div>
+        </div>
+
         <div className="m-3 rounded-xl bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 p-6 shadow-md">
           <h1 className="text-2xl sm:text-6xl py-4
                          text-transparent font-bold bg-clip-text
@@ -32,22 +95,24 @@ function Home() {
             </div>
         </div>
 
-        <div class="m-3 rounded-xl p-6 bg-gray-50 shadow-md">
-          <div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
-            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              <span class="block">Ready to dive in?</span>
-              <span class="block text-indigo-600">Start to contact me</span>
+        <div className="m-3 rounded-xl p-6 bg-gray-50 shadow-md">
+          <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              <span className="block">Ready to dive in?</span>
+              <span className="block text-indigo-600">Start to contact me</span>
             </h2>
-            <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-              <div class="inline-flex rounded-md shadow">
-                <p class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700">Contact</p>
+            <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+              <div className="inline-flex rounded-md shadow">
+                <p className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700">Contact</p>
               </div>
-              <div class="ml-3 inline-flex rounded-md shadow">
-                <p class="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50">Visite my portfilios</p>
+              <div className="ml-3 inline-flex rounded-md shadow">
+                <p className="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50">Visite my portfilios</p>
               </div>
           </div>
         </div>
       </div>
+
+
 
     </div>
       
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,9 +2,43 @@ import React from 'react'
 
 function Projects() {
   return (
-    <section className=''>
-      Projects
+    <section className="bg-gray-200 py-8">
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold mb-6 text-center">Projects</h2>
+        <div className="flex flex-wrap -mx-2">
+          <div className="w-full md:w-1/2 px-2 mb-4">
+            <div className="bg-white rounded-lg shadow-md p-6">
+              <h3 className="text-2xl font-bold mb-4">Project 1</h3>
+              <p className="text-gray-700 mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique in tortor et dignissim. Quisque non tempor leo.
+              </p>
+              <a
+                href="#"
+                className="inline-block py-2 px-4 rounded bg-blue-500 text-white font-bold hover:bg-blue-600"
+              >
+                View Project
+              </a>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 px-2 mb-4">
+            <div className="bg-white rounded-lg shadow-md p-6">
+              <h3 className="text-2xl font-bold mb-4">Project 2</h3>
+              <p className="text-gray-700 mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique in tortor et dignissim. Quisque non tempor leo.
+              </p>
+              <a
+                href="#"
+                className="inline-block py-2 px-4 rounded bg-blue-500 text-white font-bold hover:bg-blue-600"
+              >
+                View Project
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
+
+    
   )
 }
 
