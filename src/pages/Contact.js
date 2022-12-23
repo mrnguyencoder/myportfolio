42b24a090f2725050eb1183a42b85cf86c53c99e
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-23 18:12:05 +0100

    formsubmit.co

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { FaAddressCard, FaPhoneVolume } from "react-icons/fa";
 import { SiGmail } from 'react-icons/si';
-import { useForm } from "react-hook-form";
 import { motion } from 'framer-motion';
+import { useForm } from "react-hook-form";
 
 function Contact() {
-  const { handleSubmit, register} = useForm();
-  const onSubmit = values => console.log(values);
+  const {
+    register, trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (e) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.prevenDefault();
+    }
+  };
 
   return (
-    <motion.section className=''
+    <motion.section className='container'
       initial="hidden"
       whileInView="visible"
       viewport={{ one: true, amount: 0.5 }}
@@ -37,30 +46,76 @@ function Contact() {
           </div>
         </div>
         <form 
-            onSubmit={handleSubmit(onSubmit)}
+            onSubmit={onSubmit}
+            target="_blank"
+            action='https://formsubmit.co/mr.nguyencoder@gmail.com'
+            method='POST'
             className="flex flex-col space-y-2 w-fit mx-auto"
         >  
           <div className='flex space-x-2'>
-            <input placeholder='Name' className='outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-amber-300 text-gray-500 transition-all focus:border-amber-500 focus:text-gray-800' type="text"
-                {...register("name", {
-                  validate: value => value !== "admin" || "Nice try!"
-                })}
-            />
-            <input placeholder='Email' className='outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-amber-300 text-gray-500 transition-all focus:border-amber-500 focus:text-gray-800' type="email" 
-              {...register("email", {
-                required: "Required",
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: "invalid email address"
-                }
-              })}      
-            />
+            <input placeholder='Name' 
+                   className='outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-amber-300 text-gray-500 transition-all focus:border-amber-500 focus:text-gray-800' 
+                   type="text"
+                   {...register("name", {
+                    required: true,
+                    maxLength: 100,
+                  })}
+                  />
+                  {errors.name && (
+              <p className="text-red mt-1">
+                {errors.name.type === "required" && "This field is required."}
+                {errors.name.type === "maxLength" && "Max length is 100 char."}
+              </p>
+            )}
+            <input placeholder='Email' 
+                   className='outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-amber-300 text-gray-500 transition-all focus:border-amber-500 focus:text-gray-800' 
+                   type="email" 
+                   {...register("email", {
+                    required: true,
+                    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  })}     
+                  />
+                  {errors.email && (
+              <p className="text-red mt-1">
+                {errors.email.type === "required" && "This field is required."}
+                {errors.email.type === "pattern" && "Invalid email address."}
+              </p>
+            )}
           </div>
-          <input placeholder='Subject' className='outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-amber-300 text-gray-500 transition-all focus:border-amber-500 focus:text-gray-800' type="text"
-                {...register("subject")}
-          />
-          <textarea {...register("message")} placeholder='Message' className='outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-amber-300 text-gray-500 transition-all focus:border-amber-500 focus:text-gray-800'/>
-          <button type='submit' className='bg-green-400 py-5 px-10 rounded-md font-bold text-lg'>Submit</button>
+          <input placeholder='Subject' 
+                 className='outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-amber-300 text-gray-500 transition-all focus:border-amber-500 focus:text-gray-800' 
+                 type="text"
+                 {...register("subject", {
+                  required: true,
+                  maxLength: 300,
+                })}
+                />
+                {errors.name && (
+            <p className="text-red mt-1">
+              {errors.name.type === "required" && "This field is required."}
+              {errors.name.type === "maxLength" && "Max length is 300 char."}
+            </p>
+          )}
+          <textarea name="message"
+                    placeholder='Message' 
+                    className='outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-amber-300 text-gray-500 transition-all focus:border-amber-500 focus:text-gray-800'
+                    {...register("message", {
+                      required: true,
+                      maxLength: 2000,
+                    })}
+                    />
+                   {errors.message && (
+              <p className="text-red mt-1">
+                {errors.message.type === "required" &&
+                  "This field is required."}
+                {errors.message.type === "maxLength" &&
+                  "Max length is 2000 char."}
+              </p>
+            )} 
+          <button type='submit' 
+                  className='bg-green-400 py-5 px-10 rounded-md font-bold text-lg'>
+                  Send me a message
+          </button>
         </form>
      </div>
     </motion.section>
