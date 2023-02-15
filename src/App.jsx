28c3e8f128443b33cdaac086f428f46c13e52afe
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-02-15 14:17:19 +0100

    add localtime and Visited on top

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Footer from "./components/Footer"
 import Home from "./components/Home"
 import Navbar from "./components/Navbar"
 import Projects from './components/Projects'
+import Top from './components/Top'
 
 
 function App() {
   return (
     <div >
+      <Top/>
       <Navbar/>
       <Home/>
       <Projects/>
