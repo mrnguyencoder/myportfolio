5313ac6beae4e75c91afab89463c659b268d6b93
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-02-21 17:42:30 +0100

    top

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,38 +1,38 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// function Top() {
-//     const [visitsCount, setVisitsCount] = useState(0);
-//     const [localTime, setLocalTime] = useState(new Date());
+function Top() {
+    const [visitsCount, setVisitsCount] = useState(0);
+    const [localTime, setLocalTime] = useState(new Date());
   
-//     useEffect(() => {
-//       const storedVisitsCount = localStorage.getItem('visitsCount');
-//       if (storedVisitsCount) {
-//         setVisitsCount(parseInt(storedVisitsCount));
-//       }
-//       const intervalId = setInterval(() => {
-//         setLocalTime(new Date());
-//       }, 1000);
-//       return () => clearInterval(intervalId);
-//     }, []);
+    useEffect(() => {
+      const storedVisitsCount = localStorage.getItem('visitsCount');
+      if (storedVisitsCount) {
+        setVisitsCount(parseInt(storedVisitsCount));
+      }
+      const intervalId = setInterval(() => {
+        setLocalTime(new Date());
+      }, 1000);
+      return () => clearInterval(intervalId);
+    }, []);
   
-//     useEffect(() => {
-//       const handleVisit = () => {
-//         setVisitsCount(visitsCount + 1);
-//       };
-//       window.addEventListener('load', handleVisit);
-//       return () => window.removeEventListener('load', handleVisit);
-//     }, [visitsCount]);
+    useEffect(() => {
+      const handleVisit = () => {
+        setVisitsCount(visitsCount + 1);
+      };
+      window.addEventListener('load', handleVisit);
+      return () => window.removeEventListener('load', handleVisit);
+    }, [visitsCount]);
   
-//     useEffect(() => {
-//       localStorage.setItem('visitsCount', visitsCount);
-//     }, [visitsCount]);
+    useEffect(() => {
+      localStorage.setItem('visitsCount', visitsCount);
+    }, [visitsCount]);
   
-//   return (
-//     <section className='flex justify-between p-3 bg-slate-700 text-yellow-400'>
-//       <div>{localTime.toLocaleString()}</div>
-//       <div>Visited: {visitsCount}</div>
-//     </section>
-//   )
-// }
+  return (
+    <section className='flex justify-between p-3 bg-slate-700 text-yellow-400'>
+      <div>{localTime.toLocaleString()}</div>
+      <div>Visited: {visitsCount}</div>
+    </section>
+  )
+}
 
-// export default Top
+export default Top
